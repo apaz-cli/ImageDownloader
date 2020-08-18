fe4d67a81f34096a4a8d9bc81da6df80612f30e9
apaz <aarpazdera@gmail.com>
2020-08-17 20:02:31 -0500

    Fixed edge case errors

--- DownloadImages.py
+++ DownloadImages.py
@@ -1,19 +1,25 @@
-import os.path, random, shutil, sys, subprocess, pip, errno, getpass
+import requests
+import os.path
+import random
+import shutil
+import sys
+import subprocess
+import pip
+import getpass
 
 
 def get_downloads_folder_path():
     if sys.platform.startswith('win'):
-        return "C:" +  os.sep + "Users" +  os.sep + getpass.getuser()  +  os.sep + "Downloads"
+        return "C:" + os.sep + "Users" + os.sep + getpass.getuser() + os.sep + "Downloads"
     else:
         return os.path.expanduser('~/Downloads')
 
+
 # Default arguments
-target_folder_path = get_downloads_folder_path() + os.sep + "ImageDownloader Downloads"
+target_folder_path = get_downloads_folder_path() + os.sep + \
+    "ImageDownloader Downloads"
 URLs_path = get_downloads_folder_path() + os.sep + 'URLs.txt'
 
-print(target_folder_path)
-print(URLs_path)
-print(os.sep)
 
 # Overwrite arguments, use defaults, or display help and exit.
 argnum = len(sys.argv)
@@ -31,7 +37,7 @@ if argnum > 1:
     # Use default arguments or command line arguments, if they're not just using the help command.
     else:
         URLs_path = sys.argv[1]
-if argnum == 3:    
+if argnum == 3:
     target_folder_path = sys.argv[2]
 
 # If they used the default target folder, create if not exists
@@ -40,19 +46,17 @@ if target_folder_path == get_downloads_folder_path() + os.sep + "ImageDownloader
     try:
         os.mkdir(target_folder_path)
     except OSError as e:
+        import errno
         if e == errno.EEXIST:
             # Ignore exception if already exists
             pass
         elif not os.path.exists(get_downloads_folder_path()):
             print("Cannot find your default downloads folder path.")
-        
 
 
 # Fetch/verify dependencies.
 print("Verifying requests module installation.")
 subprocess.call([sys.executable, "-m", "pip", "install", "--user", "requests"])
-import requests
-
 
 
 # Load file into list
@@ -60,20 +64,31 @@ links = []
 try:
     with open(URLs_path, 'r') as filehandle:
         for line in filehandle:
-            links.append(line)    
+            links.append(line)
 except IOError as e:
     print('error', str(e))
-    pass    
+    pass
 if not links:
     print("Please provide a valid file path to a nonempty file.")
     sys.exit()
 
-# Download into the path specified with the name specified, without checking for collisions.
-def download_file(url, folder_path, file_name): 
+# Download into the path specified with the name specified, without checking for collisions. Collision detection is handled below.
+def download_file(url, folder_path, file_name):
+
+    # Deal with wack twitter extensions
+    if (file_name.endswith(':large') or file_name.endswith('_large')):
+        file_name = file_name[0:len(file_name) - 6]
+
+    # Download the file into memory    
     response = requests.get(url, stream=True)
+
+    # Copy it into a file
     with open(folder_path + os.sep + file_name, 'wb+') as out_file:
         shutil.copyfileobj(response.raw, out_file)
+    
+    # Free memory
     del response
+
 
 # Download all links, checking for collisions.
 for link in links:
@@ -85,16 +100,18 @@ for link in links:
     if link == "":
         continue
 
-    # Remove any queries 
+    # Remove any queries
     query = link.rfind('?')
     if query > 0:
-        link = link[0, query]
+        link = link[0: query]
 
     print("Fetching " + link)
 
     filename = os.path.basename(link)
     # Resolve collisions
-    while os.path.exists(filename):
-            filename += random.randint(0, 9)
-    
+    while os.path.exists(target_folder_path + os.sep + filename):
+        fsplit = filename.split('.')
+        fsplit[len(fsplit) - 2] += str(random.randint(0, 9))
+        filename = '.'.join(fsplit)
+
     download_file(link, target_folder_path, filename)
